fa320e9be8a9bc97b6b06321e4ca85287c19e1e7
Leandro Mancini de Sousa <leandro.sousa@bgmrodotec.com.br>
2019-04-09 16:47:09 -0300

    createComponent

--- src/FileHelper.ts
+++ src/FileHelper.ts
@@ -1,18 +1,80 @@
+import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as fse from 'fs-extra';
 import * as path from 'path';
+import * as changeCase from 'change-case';
+
+import { GlobalConfig } from './config/global.interface';
+import { Observable } from 'rxjs';
+import { FileConfig } from './config/files.interface';
+import { ModuleConfig } from './config/types/module-config.interface';
+import { HTMLConfig } from './config/types/html-config.interface';
+import { CSSConfig } from './config/types/css-config.interface';
 
 export class FileHelper {
-    private static assetRootDir: string = path.join(__dirname, '../../assets');
+    private static createFile = <(file: string, data: string) => Observable<{}>>Observable.bindNodeCallback(fse.outputFile);
+    private static assetRootDir: string = path.join(__dirname, '../assets');
+
+    public static createComponent(componentDir: string, componentName: string, globalConfig: GlobalConfig, config: FileConfig): Observable<string> {
+        let templateFileName = this.assetRootDir + '/templates/component.template';
+
+        let componentContent = fs.readFileSync( templateFileName ).toString()
+            .replace(/{selector}/g, componentName)
+            .replace(/{templateUrl}/g, `${componentName}.component.html`)
+            .replace(/{styleUrls}/g, `${componentName}.component.${config.css.extension}`)
+            .replace(/{className}/g, changeCase.pascalCase(componentName))
+            .replace(/{quotes}/g, this.getQuotes(globalConfig));
+
+        let filename = `${componentDir}/${componentName}.component.${config.component.extension}`;
+
+        if (config.component.create) {
+            return this.createFile(filename, componentContent)
+                .map(result => filename);
+        }
+        else {
+            return Observable.of('');
+        }
+    }
+
+    public static createHtml(componentDir: string, componentName: string, config: HTMLConfig): Observable<string> {
+        return Observable.of();
+    }
+
+    public static createCss(componentDir: string, componentName: string, config: CSSConfig): Observable<string> {
+        return Observable.of();
+    }
+
+    public static createModule(componentDir: string, componentName: string, globalConfig: GlobalConfig, config: ModuleConfig): Observable<string> {
+        return Observable.of();
+    }
+
+    public static createComponentDir(uri: any, componentName: string, globalConfig: GlobalConfig): string {
+        let contextMenuSourcePath;
+
+        if (uri && fs.lstatSync(uri.fsPath).isDirectory()) {
+            contextMenuSourcePath = uri.fsPath;
+        } else if (uri) {
+            contextMenuSourcePath = path.dirname(uri.fsPath);
+        } else {
+            contextMenuSourcePath = vscode.workspace.rootPath;
+        }
+
+        let componentDir = `${contextMenuSourcePath}`;
+        if(globalConfig.generateFolder) {
+            componentDir = `${contextMenuSourcePath}/${componentName}`;
+            fse.mkdirsSync(componentDir);
+        }
+
+        return componentDir;
+    }
 
     public static getDefaultConfig(): any {
-        // let content = fs.readFile(this.assetRootDir + '/config/config.json', 'utf8').toString();
+        let content = fs.readFileSync(this.assetRootDir + '/config/config.json').toString();
 
-        let content = fs.readFile(this.assetRootDir + '/config/config.json', 'utf8', (error, data) => {
-            // ...
-            console.log(error);
-            console.log(data);
-        });
+        return JSON.parse(content);
+    }
 
-        return content;
+    private static getQuotes(config: GlobalConfig) {
+        return config.quotes === "double" ? '"' : '\'';
     }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as _ from 'lodash';
+import * as changeCase from 'change-case';
+import { Observable } from 'rxjs';
+
 import { Config } from './config.interface';
 import { FileHelper } from './FileHelper';
 
@@ -14,11 +18,48 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', (uri) => {
 		// The code you place here will be executed every time your command is executed
 		
-		let configPrefix: String = 'ng2ComponentGenerator';
+		let configPrefix: string = 'apolloGenerator';
 		let _workspace = vscode.workspace;
 		let defaultConfig: Config = FileHelper.getDefaultConfig();
 		let userConfig: Config = <Config>_workspace.getConfiguration((configPrefix + '.config'));
-		let config: Config;
+		let _config: Config;
+
+		if (userConfig) {
+            _config = _.assign(_config, defaultConfig, userConfig) as Config;
+		}
+
+		let inputBox = Observable.from(
+            vscode.window.showInputBox(
+                {
+					prompt: 'Por favor insira o nome do component'
+				}
+			)
+		);
+
+		inputBox
+			.concatMap(val => {
+				if (val === undefined || val.length === 0) {
+					vscode.window.showErrorMessage('O nome do componente não pode está vazio.');
+				}
+
+				let componentName = changeCase.paramCase(val);
+				let componentDir = FileHelper.createComponentDir(uri, componentName, _config.global);
+
+				return Observable.forkJoin(
+					FileHelper.createComponent(componentDir, componentName, _config.global, _config.files),
+					FileHelper.createHtml(componentDir, componentName, _config.files.html),
+					FileHelper.createCss(componentDir, componentName, _config.files.css),
+					FileHelper.createModule(componentDir, componentName, _config.global, _config.files.module)
+				);
+			})
+			.do(editor => {
+				console.log('editor', editor);
+			})
+			.subscribe(() => {
+				vscode.window.setStatusBarMessage('Componente creado com sucesso!', 5000);
+			}, err => vscode.window.showErrorMessage(err.message));
+
+		
 	});
 
 	context.subscriptions.push(disposable);
